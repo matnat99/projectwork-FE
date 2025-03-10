53e1eaaa832f5b789cef46f8acf19cd691c57a33
Mattia Natale <mattianatale07@gmail.com>
2025-03-10 17:48:40 +0100

    feat: add design system

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,13 @@ import DefaultLayout from "./layouts/DefaultLayout";
 
 // Pages
 import HomePage from "./pages/HomePage";
+import DesignSystem from "./pages/DesignSystem";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/design-system" element={<DesignSystem />} />
         <Route element={<DefaultLayout />}>
           <Route index element={<HomePage />} />
         </Route>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,17 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
+import Heading from "./ui/Heading";
 
 export default function Header() {
   return (
     <header className="bg-blue-500 text-white py-4 px-6 shadow-md shadow-black">
       <div className="container mx-auto flex justify-between items-center">
-        <h1 className="text-2xl font-bold">Yuno Build</h1>
+        <Link to="/">
+          <Heading level={1}>Yuno Build</Heading>
+        </Link>
         <nav>
           <ul className="flex gap-4">
             <li className="hover:text-gray-200 cursor-pointer">
-              <NavLink to="#">Home</NavLink>
+              <NavLink to="/">Home</NavLink>
             </li>
             <li className="hover:text-gray-200 cursor-pointer">
               <NavLink to="#">Catalogo</NavLink>
