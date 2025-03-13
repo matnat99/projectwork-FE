72b7d269695980844a900ba819928dbea2b4e932
RiccardoOrlando <riccardoorlando08@gmail.com>
2025-03-13 12:45:25 +0100

    feat: add correct data into Homepage and added discounted price

--- src/components/ui/Card.jsx
+++ src/components/ui/Card.jsx
@@ -3,17 +3,29 @@ import { Link } from "react-router-dom";
 import Button from "./Button";
 
 export default function HorizontalPCCard({
-  title,
+  category,
+  cpu,
+  description,
+  discount,
+  gpu,
+  id,
   image,
-  content,
-  specs,
-  link,
+  price,
+  quantity,
+  ram,
+  title,
 }) {
+  let TotalDiscount = price;
+
+  if (discount > 0) {
+    TotalDiscount = price - (price / 100) * discount;
+  }
+
   return (
     <div className="bg-white rounded-xl shadow-md shadow-black flex h-60">
       <div className="w-1/2">
         <img
-          className="h-full object-cover rounded-l-xl"
+          className="w-full h-full object-contain p-4"
           src={`${image}.jpg`}
           alt={title}
         />
@@ -23,11 +35,23 @@ export default function HorizontalPCCard({
           <Heading level={5} className="mb-1">
             {title}
           </Heading>
-          <p className="text-xs text-gray-600 mb-1">{specs}</p>
-          <p className="text-xs line-clamp-2">{content}</p>
+          {/* <ul className="text-sm list-disc pl-5">
+            <li>{gpu}</li>
+            <li>{cpu}</li>
+            <li>{ram}</li>
+          </ul> */}
+          <p className="text-xs line-clamp-6">{description}</p>
         </div>
         <div className="text-center mt-2">
-          <Link to={link}>
+          <div>
+            <span className="text-blue-600">{`€ ${TotalDiscount}`}</span>
+            {discount > 0 && (
+              <span className="text-red-500 line-through ml-2">{`€ ${
+                price
+              }`}</span>
+            )}
+          </div>
+          <Link to={`/Yuno/${id}`}>
             <Button className="text-center text-xs py-1 px-3" size="xs">
               Scopri di più
             </Button>
