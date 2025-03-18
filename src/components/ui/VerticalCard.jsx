a725c21fa1cc6e8f1129e0ea6466dd9c3bdd93e6
Luca-Mangiacotti <lucamangiacotti100@gmail.com>
2025-03-18 09:21:11 +0100

    fix vertical card link

--- src/components/ui/VerticalCard.jsx
+++ src/components/ui/VerticalCard.jsx
@@ -75,7 +75,7 @@ export default function VerticalPCCard({
   };
 
   return (
-    <Link to={`/Yuno/${id}`}>
+    <Link to={`/Yuno/${title}`}>
       <div
         className={`bg-white rounded-xl shadow-md shadow-black max-w-xs h-full flex flex-col ${
           discount > 0 ? "border-4 border-red-500" : ""
