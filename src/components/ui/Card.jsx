11fb0a8d7e2cd3fad6f721fb0f770805ca0f2a32
Luca-Mangiacotti <lucamangiacotti100@gmail.com>
2025-03-18 09:20:55 +0100

    fix: link to title product from cards

--- src/components/ui/Card.jsx
+++ src/components/ui/Card.jsx
@@ -73,7 +73,7 @@ export default function HorizontalPCCard({
   };
 
   return (
-    <Link to={`/Yuno/${id}`}>
+    <Link to={`/yuno/${title}`}>
       <div
         className={`bg-white rounded-xl shadow-md shadow-black flex h-60 ${
           discount > 0 ? "border-4 border-red-500" : ""
