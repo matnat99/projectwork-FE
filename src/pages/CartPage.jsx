90f2ba52a5379206ebe2212818796a74d93810a1
Luca-Mangiacotti <lucamangiacotti100@gmail.com>
2025-03-20 09:29:51 +0100

    fix text color on cart

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -77,9 +77,9 @@ export default function CartPage() {
 
   return (
     <div className="container mx-auto px-4 py-8">
-      <div className="text-white mt-4">
+      <div className="mt-4 text-white">
         <Heading level={2}>Carrello</Heading>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-4 text-black">
           {cart.map((item) => (
             <div
               key={item.id}
